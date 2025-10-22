12ca6d795c0ba1373d84c862908e7785147779a5
cludley <cludley@anamosa.k12.ia.us>
2025-10-22 16:01:29 +0000

    added the code

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-var automobiles = ["car", "truck", "motorcycle"]
+const automobiles = ['car', 'truck', 'motorcycle'];
+automobiles.push('airplane', 'skateboard');
+automobiles.unshift('helicopter', 'bike');
 
-var automobilesCopy =
+const automobilesCopy = `The current list of vehicles is: ${automobiles}`;
+automobiles.pop();
+automobiles.shift();
+
+document.getElementById('result').innerHTML = automobiles
