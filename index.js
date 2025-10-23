455145933befc40604574a08d2d4b44bb4021453
cludley <cludley@anamosa.k12.ia.us>
2025-10-23 15:53:03 +0000

    m

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const automobiles = ['car', 'truck', 'motorcycle'];
 automobiles.push('airplane', 'skateboard');
 automobiles.unshift('helicopter', 'bike');
 
-const automobilesCopy = `The current list of vehicles is: ${automobiles}`;
 automobiles.pop();
 automobiles.shift();
+const automobilesCopy = `The current list of vehicles is: ${automobiles}`;
 
-document.getElementById('result').innerHTML = automobiles
+document.getElementById('result').innerHTML = automobiles;
